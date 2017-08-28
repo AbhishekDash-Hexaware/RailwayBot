b80c5fe8a6d3244f3ca011c8ab7357f0183be341
pourabk-hexaware <36023@Hexaware.com>
2017-08-29 00:55:16 +0530

    Updates

--- response/pnr.js
+++ response/pnr.js
@@ -5,11 +5,12 @@ module.exports={
         chart_prepared="has been prepared.";
       }
       else if(chart_prepared==false){
-        chart_prepared="is still unprepared as of now.";
+        chart_prepared="is still not prepared.";
       }
-      var messageTwo="The chart for this "+chart_prepared+" View more specific details below.";
-      var messageOne="I see that you're travelling from "+from_station_name+" to "+reservation_upto_name+" for "+doj+".";
-      var trainDetailsSubtitle="Train Name:"+train_name+"\nTrain Number:"+train_number+"\nCoach Class:"+journey_class;
+      var messageTwo="The chart for this train "+chart_prepared+" View more specific details below.";
+      var messageOne="I see that you're travelling from "+from_station_name+" to "+reservation_upto_name+" on "+doj+".";
+      var journeySubtitle="From : "+from_station_name+"\nTo : "+reservation_upto_name+"\nDate : "+doj;
+      var trainDetailsSubtitle="Train Name : "+train_name+"\nTrain Number : "+train_number+"\nCoach Class : "+journey_class;
       var facebookResponse={
                               "speech": "",
                               "displayText": "",
@@ -21,6 +22,27 @@ module.exports={
                                 {
                                   "text":messageTwo
                                 },
+                                {
+                                  "attachment": {
+                                    "type": "template",
+                                    "payload": {
+                                      "template_type": "generic",
+                                      "elements": [
+                                        {
+                                          "title": "Journey Details",
+                                          "subtitle": trainDetailsSubtitle,
+                                          "buttons": [
+                                            {
+                                              "type": "postback",
+                                              "title": "More Info",
+                                              "payload": "more_pnr_info"
+                                            }
+                                          ]
+                                        },
+                                      ]
+                                     }
+                                    }
+                                  },
                                 {
                                   "attachment": {
                                     "type": "template",
@@ -41,6 +63,36 @@ module.exports={
                                       ]
                                      }
                                     }
+                                  },
+                                  {
+                                      "text":"Is there anything else?",
+                                      "quick_replies":[
+                                      {
+                                        "content_type":"text",
+                                        "title":"More Detailed Info",
+                                        "payload":"more_pnr_info"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"Try Another PNR",
+                                        "payload":"pnr_status"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"Find Trains",
+                                        "payload":"find_train"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"Another Query",
+                                        "payload":"another_query"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"That's all",
+                                        "payload":"thanks"
+                                      }
+                                    ]
                                   }
                                 ]
                               },
@@ -61,11 +113,11 @@ module.exports={
         let trainDetailsSubtitle ;
         console.log("Printing raw data",bookingstatus);
         console.log("Printing splited data",bookingarray[0],bookingarray[1],bookingarray[2])
-        
+
         if (bookingarray[1]=="-"){
-        trainDetailsSubtitle="Status: "+bookingarray[0]+"\nSeat/Berth No: "+bookingarray[2]+"\nCurrent Status: "+currentstatus;  
+        trainDetailsSubtitle="Status: "+bookingarray[0]+"\nSeat/Berth No: "+bookingarray[2]+"\nCurrent Status: "+currentstatus;
         }else{
-        trainDetailsSubtitle="Status: "+bookingarray[0]+"\nCoach: "+bookingarray[1]+"\nSeat/Berth No: "+bookingarray[2]+"\nCurrent Status: "+currentstatus;  
+        trainDetailsSubtitle="Status: "+bookingarray[0]+"\nCoach: "+bookingarray[1]+"\nSeat/Berth No: "+bookingarray[2]+"\nCurrent Status: "+currentstatus;
         }
       let title="Passenger "+(i+1)+" Booking Status";
         dynamicBody.push({
@@ -79,7 +131,7 @@ module.exports={
                               "speech": "",
                               "displayText": "",
                               "data": {
-                                "facebook": {
+                                "facebook": [{
                                                 "attachment": {
                                                   "type": "template",
                                                   "payload": {
@@ -87,7 +139,32 @@ module.exports={
                                                     "elements": dynamicBody
                                                    }
                                                   }
-                                                }
+                                                },
+                                                {
+                                                    "text":"Is there anything else?",
+                                                    "quick_replies":[
+                                                    {
+                                                      "content_type":"text",
+                                                      "title":"Try Another PNR",
+                                                      "payload":"pnr_status"
+                                                    },
+                                                    {
+                                                      "content_type":"text",
+                                                      "title":"Find Trains",
+                                                      "payload":"find_train"
+                                                    },
+                                                    {
+                                                      "content_type":"text",
+                                                      "title":"Another Query",
+                                                      "payload":"another_query"
+                                                    },
+                                                    {
+                                                      "content_type":"text",
+                                                      "title":"That's all",
+                                                      "payload":"thanks"
+                                                    }
+                                                  ]
+                                                }]
                               },
                               "contextOut": [],
                               "source": "DuckDuckGo"
