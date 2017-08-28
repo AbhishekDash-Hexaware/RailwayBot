7e449703ed527aeeeaf723a4de06713627bab740
pourabk-hexaware <36023@Hexaware.com>
2017-08-29 00:31:29 +0530

    Updates

--- response/trainavailable.js
+++ response/trainavailable.js
@@ -4,19 +4,19 @@ module.exports = {
     var dynamicBody=[];
 
     var messageOne = "There are "+train_number.length+" trains available.";
-
+    var messageTwo = "Is there anything else?";
     //checking data
     // console.log(train_number);
     // console.log(train_name);
     // console.log(train_travel_time);
     // console.log(train_schedule_arrival);
     // console.log(train_schedule_departure);
-    
+
 
     // train_cls.forEach(function(element) {
     //  element.forEach(function(cls){
     //    cls=cls.slice(0,1)+" "+cls.slice(1);
-    //  }) 
+    //  })
     // });
      for(var i=0;i<train_cls.length;i++){
        for(j=0;j<train_cls[i].code_data.length;j++){
@@ -27,14 +27,14 @@ module.exports = {
 console.log(train_cls);
 
     for(var i=0;i<train_number.length;i++){
-      
+
       var stringifiedClass = train_cls[i].code_data.join(",");
       console.log(stringifiedClass);
       var title = train_name[i];
       var train_payload = train_number[i]+" seat "+stringifiedClass;
       console.log("Quick Reply Payload Set as : "+train_payload);
       var trainDetailsSubtitle = "No.: "+train_number[i]+" Travel Time: "+train_travel_time[i]+"\nDeparture: "+train_schedule_departure[i]+"\nArrival: "+train_schedule_arrival[i];
-      
+
       dynamicBody.push({
         "title": title,
         "subtitle": trainDetailsSubtitle,
@@ -58,14 +58,39 @@ console.log(train_cls);
                                 "text":messageOne
                               },
                               {
-                                              "attachment": {
-                                                "type": "template",
-                                                "payload": {
-                                                  "template_type": "generic",
-                                                  "elements": dynamicBody
-                                                 }
-                                                }
-                                              }]
+                                  "attachment": {
+                                    "type": "template",
+                                    "payload": {
+                                      "template_type": "generic",
+                                      "elements": dynamicBody
+                                     }
+                                    }
+                                  },
+                                  {
+                                      "text":messageTwo,
+                                      "quick_replies":[
+                                      {
+                                        "content_type":"text",
+                                        "title":"Another Transit",
+                                        "payload":"find_train"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"PNR Status",
+                                        "payload":"pnr_status"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"Another Query",
+                                        "payload":"another_query"
+                                      },
+                                      {
+                                        "content_type":"text",
+                                        "title":"That's all",
+                                        "payload":"thanks"
+                                      }
+                                    ]
+                                  }]
                             },
                             "contextOut": [],
                             "source": "DuckDuckGo"
