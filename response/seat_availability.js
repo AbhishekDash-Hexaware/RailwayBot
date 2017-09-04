94b8d3dca4b2a6688d6a261b880762141bb5d377
AbhishekDash-Hexaware <abhishekda@hexaware.com>
2017-09-04 23:18:23 +0530

    error handling in price null

--- response/seat_availability.js
+++ response/seat_availability.js
@@ -5,6 +5,11 @@ module.exports={
     var class_name=multidata[0].cls;
     var quota_name=multidata[0].quota;
     var price=multidata[1].price;
+
+    if(train_name =="UNAVAILABLE"){
+      var flag=1;
+    }
+
     var messageOne="Here are the available seats in the "+train_name+" for the next 5 days.";
     //console.log("forming");
     let dynamicBody=[];
@@ -19,7 +24,47 @@ module.exports={
         })
     }
 
+  if(flag==1){
+    var messageOne="It seems the seats available for this train is unavailable at the moment. Try for this train later or go ahead for another train."
+      var facebookResponse={
 
+          "speech": "",
+          "displayText": "",
+          "data": {
+            "facebook": [{
+              "text":messageOne
+            },
+              {
+                  "text":"Is there anything else?",
+                  "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Find Another Train",
+                    "payload":"find_train"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Check PNR Status",
+                    "payload":"pnr_status"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Another Question",
+                    "payload":"another_query"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"That's all",
+                    "payload":"thanks"
+                  }
+                ]
+              }]
+            },
+          "contextOut": [],
+          "source": "DuckDuckGo"
+          }
+  }
+  else{
   var facebookResponse={
 
           "speech": "",
@@ -65,6 +110,7 @@ module.exports={
           "contextOut": [],
           "source": "DuckDuckGo"
           }
+  }
 
     response.send(facebookResponse);
   //  console.log("sent");
